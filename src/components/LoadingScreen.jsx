fb8fc301d4b3367f71477f1a5ef5614b69da7a08
0hrawat <dev.ankurawat@gmail.com>
2025-05-16 23:11:25 +0530

    add basic structure for one-time loading screen

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,98 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const LoadingScreen = () => {
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.to("#loding1", {
+      duration: 2,
+      scale: 1,
+      ease: "power2.inOut",
+      opacity: 1,
+      zIndex: 10,
+      delay: 0.2,
+      onComplete:()=>{
+        gsap.to("#text1",{
+            delay:-1,
+         scale:1.5,   
+            duration:0.1,
+        })
+      }
+    })
+      tl.to("#loding2", {
+          duration: 2,
+          scale: 1,
+          ease: "power2.inOut",
+          opacity: 1,
+          zIndex: 20,
+          delay: 0.2,})
+          tl.to("#loding3", {
+              duration: 2.5,
+              ease: "power2.inOut",
+              opacity: 1,
+              zIndex: 30,
+              delay: 0.2,
+              scale: 1.2,
+              width: "100%",
+              height: "100%",
+              top: "0%",
+              left: "0%",
+              transform: "translate(0%, 0%)",})
+    // gsap.to("#loding1", {
+    //   duration: 2,
+    //   scale: 1,
+    //   ease: "power2.inOut",
+    //   opacity: 1,
+    //   zIndex: 10,
+    //   delay: 0.2,
+    //   onComplete: () => {
+    //     gsap.to("#loding2", {
+    //       duration: 2,
+    //       scale: 1,
+    //       ease: "power2.inOut",
+    //       opacity: 1,
+    //       zIndex: 20,
+    //       delay: 0.2,
+    //       onComplete: () => {
+    //         gsap.to("#loding3", {
+    //           duration: 2.5,
+    //           ease: "power2.inOut",
+    //           opacity: 1,
+    //           zIndex: 30,
+    //           delay: 0.2,
+    //           scale: 1.2,
+    //           width: "100%",
+    //           height: "100%",
+    //           top: "0%",
+    //           left: "0%",
+    //           transform: "translate(0%, 0%)",
+              
+    //         });
+    //       },
+    //     });
+    //   },
+    // });
+  });
+  return (
+    <div className="loadings relative w-full h-screen bg-black overflow-hidden">
+      <div
+        id="loding1"
+        className="loding z-30 opacity-0 scale-0 bg-[#695eff] flex items-center justify-center absolute w-[80%] h-[80vh] top-[50%] left-[50%] -translate-1/2 rounded-4xl overflow-hidden p-3"
+      >
+        <h1 id="text1" className="text-8xl text-white">Write Your Notes ✍️</h1>
+      </div>
+      <div
+        id="loding2"
+        className="loding scale-0 opacity-0  z-20 bg-[#ffc703] flex items-center justify-center absolute w-[80%] h-[80vh] top-[50%] left-[50%] -translate-1/2 rounded-4xl overflow-hidden p-3"
+      >
+        <h1 className="text-8xl text-white">Drag & Drop Easily 📌</h1>
+      </div>
+      <div
+        id="loding3"
+        className="loding scale-0 opacity-0 z-10 bg-white flex items-center justify-center  absolute w-[80%] h-[80vh] top-[50%] left-[50%] -translate-1/2 rounded-4xl overflow-hidden p-3"
+      ></div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoadingScreen from '../components/LoadingScreen'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-gray-400'><LoadingScreen/></div>
+  )
+}
+
+export default Home
